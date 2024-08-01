abba576bb48a0f20bd2436d006a4e02eaea54551
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-01 21:03:43 +0200

    added the footer

---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+export default function Footer() {
+  return (
+    <p>
+      This Weather App coded by
+      <a href="https://github.com/kabul2022" target="blank">
+        Aziza Rezaie
+      </a>{" "}
+      it's,"
+      <a
+        href="https://github.com/kabul2022/react-weather-homework"
+        target="blank"
+      >
+        on GitHub
+      </a>{" "}
+      and hosted
+      <a
+        href="https://master--shecodereact.netlify.app/overview"
+        target="blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+    </p>
+  );
+}
