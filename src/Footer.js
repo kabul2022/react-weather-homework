e5e8cee4c812cb60a1e74e00bd8ac32cf21839f6
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-02 09:31:47 +0200

    Edited the git link

--- src/Footer.js
+++ src/Footer.js
@@ -8,8 +8,10 @@ export default function Footer() {
         Aziza Rezaie
       </a>{" "}
       it's,
-      <a href="https://master--shecodereact.netlify.app/">on GitHub</a> and
-      hosted
+      <a href="https://github.com/kabul2022/react-weather-homework">
+        on GitHub
+      </a>{" "}
+      and hosted
       <a
         href="https://master--candid-salamander-91c8a3.netlify.app/"
         target="blank"
